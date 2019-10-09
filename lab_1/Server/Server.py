e832059eb7f4bcaf58f66343f1aa241d1384614a
Danil <Dankwaso@gmail.com>
2019-10-09 12:11:50 +0300

    Update Server.py

--- lab_1/Server/Server.py
+++ lab_1/Server/Server.py
@@ -1,26 +1,27 @@
-from lab_1.Crawler.Finder import get_html_page, find_articles, publish_report
+from lab_1.Crawler.Finder import find_articles, publish_report
 import datetime
-import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
-@app.route('/')
 
+
+@app.route('/')
 def server_run():
     url3 = "https://stopgame.ru/news"
     now = datetime.datetime.now()
-    resp3 = get_html_page(url3)
     top = find_articles(url3)
     publish_report(top)
-    art=[]
+    art = []
     for article in top['articles']:
         art.extend(article.values())
-    return render_template('news.html', url = url3, date = now, articles = art)
+    return render_template('news.html', url=url3, date=now, articles=art)
+
 
 @app.route('/<cmd>')
-def refresh():
+def refresh(cmd=None):
     if cmd == "Refresh Page":
         server_run()
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port=9090)
