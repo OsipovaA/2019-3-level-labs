d4b466ad7384d8fd369bfd095863be1a5b01b7df
Danil <Dankwaso@gmail.com>
2019-10-09 12:11:16 +0300

    Update main.py

--- lab_1/Crawler/main.py
+++ lab_1/Crawler/main.py
@@ -1,9 +1,9 @@
 from lab_1.Crawler.Finder import get_html_page, find_articles, publish_report
-url3="https://stopgame.ru/news"
+
+url3 = "https://stopgame.ru/news"
 resp3 = get_html_page(url3)
 top = find_articles(url3)
 publish_report(top)
-art=[]
+art = []
 for article in top['articles']:
     art.extend(article.values())
-print(art)
