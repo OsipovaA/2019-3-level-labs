6a46965afa74c17e6461dfef1771b1d6e8ba0c8a
Danil <Dankwaso@gmail.com>
2019-10-09 11:36:58 +0300

    Update main.py

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,4 +1,4 @@
-from Lab1.Finder import get_html_page, find_articles, publish_report
+from lab_1.Finder import get_html_page, find_articles, publish_report
 
 url3="https://stopgame.ru/news"
 resp3 = get_html_page(url3)
