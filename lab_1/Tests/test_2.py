cabc03a0c3af92c6b1abbe1ac4cc91e267bd144b
Danil <Dankwaso@gmail.com>
2019-10-13 20:24:16 +0300

    Update test_2.py

--- lab_1/Tests/test_2.py
+++ lab_1/Tests/test_2.py
@@ -1,25 +1,22 @@
 import unittest
-import codecs
-import urllib.request
-from bs4 import BeautifulSoup
+from lab_1.Crawler.Finder import find_articles
+import requests
+
+
+'''Test articles not empty'''
 
 class Second_test(unittest.TestCase):
     def test_crawler(self):
-        urllib.request.urlretrieve("https://stopgame.ru/news", "test.txt")
-
-        with codecs.open("test.txt", "r", encoding="utf-8") as outfile:
-            topics = {}
-            topics["articles"] = []
-            soup3 = BeautifulSoup(outfile, 'html.parser')
-            l3 = soup3.find("div", {"class": "lent-left"})
-            for i in l3.findAll("div", "title lent-title"):
-                topics["articles"].append({"Title": i.text})
-        c=0
-        list_len = len(topics["articles"])
+        url = 'https://stopgame.ru/news'
+        resp = requests.get(url) #Status
+        articl = find_articles(url, resp)#Parcer
+        c = 0
+        list_len = len(articl["articles"])#length
         while c < list_len:
-            art_list = list(topics["articles"][c].values())
-            self.assertTrue(art_list)
-            c+=1
+            art_list = list(articl["articles"][c].values())#titles
+            self.assertTrue(art_list)#titles in parcer list not empty
+            c += 1
+
 
 if __name__ == '__main__':
     unittest.main()
