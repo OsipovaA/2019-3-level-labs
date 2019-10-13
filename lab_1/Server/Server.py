cb62fafbeda3769f749b14206e41f9b3964be922
Danil <Dankwaso@gmail.com>
2019-10-13 20:22:31 +0300

    Update Server.py

--- lab_1/Server/Server.py
+++ lab_1/Server/Server.py
@@ -1,7 +1,7 @@
 from lab_1.Crawler.Finder import find_articles, publish_report
-import datetime
 import codecs
 import json
+import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -10,23 +10,13 @@ app = Flask(__name__)
 @app.route('/')
 def server_run():
     url3 = "https://stopgame.ru/news"
-    now = datetime.datetime.now()
-    top = find_articles(url3)
+    resp = requests.get(url3)
+    top = find_articles(url3, resp)
     publish_report(top)
     with codecs.open("StopGame.json", "r", encoding="utf-8") as outfile:
         jdata = json.load(outfile)
-        urls=jdata['url']
-        now_time=jdata['creationDate']
-        eds=jdata['articles']
-        list_len = len(eds)
-        art = []
-        i = 0
-        while i < list_len:
-            art_list = list(jdata["articles"][i].values())
-            art.extend(art_list)
-            i += 1
         outfile.close()
-    return render_template('news.html', url=urls, date=now_time, articles=art)
+    return render_template('news.html', articles=jdata)
 
 
 @app.route('/<cmd>')
