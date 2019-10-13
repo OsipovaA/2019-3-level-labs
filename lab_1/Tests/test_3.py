a5c45c1282fd29ac2d26f3bdad8e42bc683e96bf
Danil <Dankwaso@gmail.com>
2019-10-13 20:24:37 +0300

    Update test_3.py

--- lab_1/Tests/test_3.py
+++ lab_1/Tests/test_3.py
@@ -1,11 +1,15 @@
-import  unittest
-import requests
+from lab_1.Crawler.Finder import get_html_page
+import unittest
+
+'''URL status'''
+
 
 class Third_test(unittest.TestCase):
     def test_url(self):
         url = "https://stopgame.ru/news"
-        resp = requests.get(url)
-        self.assertEqual(resp.status_code,200)
+        resp = get_html_page(url)
+        self.assertEqual(resp.status_code, 200) # status code == 200 or not
+
 
 if __name__ == '__main__':
     unittest.main()
