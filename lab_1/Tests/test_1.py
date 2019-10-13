a594c7bbc8d39b599b4c9864e8c31005d05d5a9a
Danil <Dankwaso@gmail.com>
2019-10-13 20:23:58 +0300

    Update test_1.py

--- lab_1/Tests/test_1.py
+++ lab_1/Tests/test_1.py
@@ -1,45 +1,24 @@
+from lab_1.Crawler.Finder import get_html_page, publish_report, find_articles
 import unittest
 import json
 import codecs
 
+'''Test file structure'''
+
+
 class First_test(unittest.TestCase):
 
     def test_url(self):
-        with codecs.open("StopGame.json", "r", encoding="utf-8") as json_data:
-            jdata = json.load(json_data)
-            eds = list(jdata.keys())
-            ed = eds[0]
-
-            self.assertEqual(str(ed), 'url')
-        json_data.close()
-
-    def test_data(self):
-        with codecs.open("StopGame.json", "r", encoding="utf-8") as json_data:
-            jdata = json.load(json_data)
-            eds = list(jdata.keys())
-            ed = eds[1]
-            self.assertEqual(str(ed), 'creationDate')
-        json_data.close()
-
-    def test_articles(self):
-        with codecs.open("StopGame.json", "r", encoding="utf-8") as json_data:
-            jdata = json.load(json_data)
-            eds = list(jdata.keys())
-            ed = eds[2]
-            self.assertEqual(str(ed), 'articles')
-        json_data.close()
-
-    def test_titles(self):
-        with codecs.open("StopGame.json", "r", encoding="utf-8") as json_data:
-            jdata = json.load(json_data)
-            eds = list(jdata.values())
-            list_len = len(eds[2])
-            i = 0
-            while i < list_len:
-                art_list = list(jdata["articles"][i].keys())
-                art_single = art_list[0]
-                self.assertEqual(str(art_single), 'Title')
-                i += 1
+        url3 = "https://stopgame.ru/news"
+        resp3 = get_html_page(url3)#status
+        top = find_articles(url3, resp3)#Parcer
+        publish_report(top)#write json
+        with codecs.open("StopGame.json", "r", encoding="utf-8") as json_data:#open json
+            jdata = json.load(json_data)#load json
+            self.assertEqual(jdata['url'][0], url3)#url ink == url link(https://stopgame.ru/news" == https://stopgame.ru/news")
+            for article in jdata["articles"]:
+                for art in article.values():
+                    self.assertTrue(art)#titles in son not empty
         json_data.close()
 
 
